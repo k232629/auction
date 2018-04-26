775c6d27f119c019cc4c8dd6b2a51d8f7c97274f
k232629 <olhakaraiman@gmail.com>
2018-04-26 12:56:24 +0300

    test

--- src/main/java/com/karaiman/shoppingcart/config/WebSecurityConfig.java
+++ src/main/java/com/karaiman/shoppingcart/config/WebSecurityConfig.java
@@ -33,6 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
  
    @Override
    protected void configure(HttpSecurity http) throws Exception {
+	   // test
  
       http.csrf().disable();
  
